1ad345e1bf39ac58fdd294f5830f3fd0d3eb92cf
baba-s <shotababa1206@gmail.com>
2022-08-28 14:19:26 +0900

    add

---
+++ Editor/PackageManagerViewKoganePackageOnGitHubExtension.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal sealed class PackageManagerViewKoganePackageOnGitHubExtension
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private Button      m_button;
+        private PackageInfo m_selectedPackageInfo;
+
+        static PackageManagerViewKoganePackageOnGitHubExtension()
+        {
+            var extension = new PackageManagerViewKoganePackageOnGitHubExtension();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_button = new Button
+            (
+                () =>
+                {
+                    var url = $"https://github.com/baba-s/{m_selectedPackageInfo.displayName}";
+                    url = url.Replace( "Kogane ", "Kogane." );
+                    url = url.Replace( " ", "" );
+                    Application.OpenURL( url );
+                }
+            )
+            {
+                text = "View on GitHub",
+            };
+
+            return m_button;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            m_selectedPackageInfo = packageInfo;
+            m_button.visible      = packageInfo.author.name == "baba-s";
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.PackageManagerViewKoganePackageOnGitHubExtension
+# Kogane Package Manager View Kogane Package On GitHub Extension
+
+com.baba-s のパッケージを GitHub で開くボタンを Package Manager に追加するエディタ拡張
